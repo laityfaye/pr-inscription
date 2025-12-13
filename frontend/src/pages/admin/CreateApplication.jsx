ea4345ff71c9c8429774cad2e3fb245c3ea253c3
Laity Faye <laity.faye@univ-thies.sn>
2025-12-13 02:50:16 +0000

    avocat

--- frontend/src/pages/admin/CreateApplication.jsx
+++ frontend/src/pages/admin/CreateApplication.jsx
@@ -661,7 +661,7 @@ const CreateApplication = () => {
                       </div>
                     </div>
                   </div>
-                }
+                )}
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {[
